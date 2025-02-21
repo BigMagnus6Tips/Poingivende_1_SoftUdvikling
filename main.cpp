9c8b0f60856ef3059a38cc6e99dcc00d17e76d64
BigMagnus6Tips <magne.jacobsen04@gmail.com>
2025-02-21 11:42:26 +0100

    AdTask done

--- main.cpp
+++ main.cpp
@@ -10,6 +10,10 @@ class Task
 	Task(std::string description){
 		_description = description;
 	}
+	std::string getDescription()
+	{
+		return _description;
+	}
 };
 
 class TodoList
@@ -18,11 +22,25 @@ class TodoList
 	public:
 	std::vector<Task> todolist;
 	TodoList(){}
+	void addTask(Task task)
+	{
+		todolist.push_back(task);
+
+	}
+	Task getTaskAt(int i) 
+	{
+		return todolist.at(i);
+
+	}
+
+    
 };
 
 int main() {
 
     TodoList todo = TodoList();
-
+	Task task1 = Task("Get groceries");
+	todo.addTask(task1);
+	std::cout << todo.getTaskAt(0).getDescription();
     return 0;
 }
