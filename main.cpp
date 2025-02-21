eee9447d065624e127867e7b4cbae9ab88f30f65
Rasmus <rasmus_kildeberg@outlook.dk>
2025-02-21 12:32:33 +0100

    Added dueDate

--- main.cpp
+++ main.cpp
@@ -7,12 +7,19 @@ class Task
 {
 	public:
 	std::string _description;
-	Task(std::string description){
+	int dueDate; // YYYYMMDD
+	Task(std::string description, int duedate){
 		_description = description;
+		dueDate = duedate;
 	}
 	std::string getDescription()
 	{
 		return _description;
+	}
+
+	int getDueDate()
+	{
+		return dueDate;
 	}
 };
 
@@ -32,18 +39,29 @@ class TodoList
 		todolist.erase(todolist.begin() + i);
 	}
 
-  void addTask(Task task)
-  {
+  	void addTask(Task task)
+  	{
 		todolist.push_back(task);
 
+	}
+
+	void outputAllTasks()
+	{
+
+		for (Task& i: todolist) {
+			std::cout << "Description: " << i.getDescription() << "due date: " << i.getDueDate() << std::endl;
+
+		}
 	}
 };
 
 int main() {
 
     TodoList todo = TodoList();
-	Task task1 = Task("Get groceries");
+	Task task1 = Task("Get groceries", 20501202);
+	Task task2 = Task("Do laundry", 20501203);
 	todo.addTask(task1);
-	std::cout << todo.getTaskAt(0).getDescription();
+	todo.addTask(task2);
+	todo.outputAllTasks();
     return 0;
 }
